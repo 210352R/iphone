c6bcc0916a7cf109c4df01a3fc5997d935e2f6b9
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-06-08 12:09:36 +0530

    added Project V1.0

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./componets/Hero";
+import Highlights from "./componets/Highlights";
+import Navbar from "./componets/Navbar";
+
+function App() {
+  return (
+    <>
+      <main>
+        <Navbar />
+        <Hero />
+        <Highlights />
+      </main>
+    </>
+  );
+}
+
+export default App;
